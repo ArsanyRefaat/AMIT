027b6d1a9edba277dbeee71922af89d33969e951
arsanyak19404-ctrl <arsanyak19@gmail.com>
2026-06-16 14:50:49 +0300

    Remove keep-alive to avoid Render TOS violation - use cache instead

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,13 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { warmApi } from '@/lib/api'
-import { startKeepAlive } from '@/lib/keepAlive'
 
-// Initial warm-up ping
 warmApi()
-
-// Start keep-alive pinger to prevent server from sleeping
-startKeepAlive()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
